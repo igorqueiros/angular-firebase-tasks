321286ad7d29110511cefc81c8580367d6f17c7c
Igor <igor.queiros@doxti.com.br>
2018-04-26 08:51:52 -0300

    commit final

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { TaskService } from './task.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule} from 'angularfire2/firestore';
+import { MatToolbarModule, MatListModule, MatLineModule, MatSlideToggleModule, MatButtonModule, MatIconModule, MatDialogModule, MatInputModule, MatFormFieldModule, MatMenuModule, MatProgressSpinnerModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { TaskItemComponent } from './task-item/task-item.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskItemComponent,
+    TaskListComponent,
+    TaskDialogComponent
+  ],
+  entryComponents:[
+    TaskDialogComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule.enablePersistence(),
+    FormsModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatLineModule,
+    MatListModule,
+    MatMenuModule,
+    MatProgressSpinnerModule,
+    MatSlideToggleModule,
+    MatToolbarModule
+  ],
+  providers: [
+    TaskService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCKA2yHBWOdRmcPhkZpKsXNJKIjns66SAU",
+    authDomain: "angular-tasks-9e8af.firebaseapp.com",
+    databaseURL: "https://angular-tasks-9e8af.firebaseio.com",
+    projectId: "angular-tasks-9e8af",
+    storageBucket: "angular-tasks-9e8af.appspot.com",
+    messagingSenderId: "416441391130"
+  }
+};
